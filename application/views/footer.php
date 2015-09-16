328d703749ea061a3635e01e700f1fc121b5924b
han <897475686@qq.com>
2015-09-16 10:22:12 +0800

    '9-16'

--- application/views/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-	Copyright &copy; BBM 2014-1015 by COD Team.
-</footer>
-</body>
-</html>
--- application/views/test/update.php
+++
@@ -1,4 +0,0 @@
-<form method="post" action="<?php echo base_url()?>account/main/update_userinfo">
-	<input type="text" name="name" />
-	<input type="submit"  />
-</form>
